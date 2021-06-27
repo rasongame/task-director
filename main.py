012ce4fb0421877ccb81703ecaff4cc9b5200c05
dubstepgunser@mail.ru <dubstepgunser@mail.ru>
2021-06-27 22:51:55 +0500

    ...

--- main.py
+++ main.py
@@ -3,18 +3,28 @@ import TableIt
 import tkinter as tk
 from datetime import datetime
 from table_interact import *
+
 class NewNoteFrame(tk.Frame):
 	def __init__(self, parent):
 		super().__init__(parent)
+		self.is_multiline = False
 		self.master = parent
-		self.pack()
+		# self.pack(side="bottom")
 		self.lbl = tk.Label(self, text="введи текст заметки")
 		self.entry = tk.Entry(self)
+		self.entry.bind("<Return>", self.add_note)
+		self.entry.bind("<Delete>", parent.check_can_remove_selected)
+		# bind for <Return> key
+		self.grid(sticky=tk.N)
 		self.btn = tk.Button(self, text="добавить", command=self.add_note)
-		self.lbl.pack(side="left")
-		self.entry.pack(side="left")
-		self.btn.pack(side="right")
-	def add_note(self):
+		self.lbl.grid(row=1, column=1)
+		self.entry.grid(row=1, column=2)
+		self.btn.grid(row=1, column=3)
+		# self.lbl.grid(side="left")
+		# self.entry.grid(side="left")
+		# self.btn.grid(side="right")
+
+	def add_note(self, *args):
 		nid = add_entry_to_table(datetime.now().strftime("%d %B %Y %I:%M%p"), self.entry.get())
 		Note(self.master, self.entry.get(), datetime.now().strftime("%d %B %Y %I:%M%p"), nid)
 
@@ -24,14 +34,18 @@ class Note(tk.Frame):
 		super().__init__(parent)
 		self.master = parent
 		self.parent = parent
-		self.pack()
-		self.lbl = tk.Label(self, text=text)
+		self.grid()
+		self.lbl = tk.Label(self, text=text, wraplength=1000)
 		self.id = nid
 		self.btn = tk.Button(self)
 		self.btn["text"] = "удалить"
 		self.btn['command'] = self.remove
-		self.lbl.pack(side="left")
-		self.btn.pack(side="right")
+		self.is_checked = tk.IntVar()
+		self.chk = tk.Checkbutton(self, variable=self.is_checked)
+		self.chk.grid(row=2, column=1)
+		self.lbl.grid(row=2, column=2)
+		self.btn.grid(row=2, column=3)
+
 	def save(self):
 		nid = add_entry_to_table(datetime.now().strftime("%d %B %Y %I:%M%p"), self.btn['text'])
 		print(f'saved {nid}')
@@ -42,13 +56,18 @@ class Note(tk.Frame):
 		self.destroy()
 
 class App(tk.Frame):
+	def check_can_remove_selected(self, *args):
+		buttons = list(filter(lambda task: task.is_checked.get(), self.tasks))
+		if len(buttons) > 1:
+			[note.remove() for note in buttons]
+
 	def __init__(self, master=None):
 		self.tasks = list()
 		super().__init__(master)
 		self.db_file = "table.db"
 		self.master = master
-		self.pack()
 		self.create_widgets()
+		
 	def load_tasks(self):
 		tasks = get_entries()
 		for task in tasks:
@@ -56,72 +75,20 @@ class App(tk.Frame):
 			self.tasks.append(note)
 
 	def create_widgets(self):
-		self.load_tasks()
-		NewNoteFrame(self)
-		# self.hi_there = tk.Button(self)
-		# self.hi_there["text"] = "Hello World\n(click me)"
-		# self.hi_there["command"] = self.say_hi
-		# self.hi_there.pack(side="top")
-
 		self.quit = tk.Button(self, text="QUIT", fg="red",
 							  command=self.master.destroy)
-		self.quit.pack(side="bottom")
+		self.quit.grid()
+		self.entry = NewNoteFrame(self)
+		self.load_tasks()
+
+		self.grid()
+		# self.pack()
 
 	def say_hi(self):
 		print("hi there, everyone!")
 
 root = tk.Tk()
+tk.Grid.rowconfigure(root, 0, weight=1)
+tk.Grid.columnconfigure(root, 0, weight=3)
 app = App(master=root)
 app.mainloop()
-
-# def start_menu_selector() -> str:
-
-#     text = """
-# 1) Прочитать задачи.
-# 2) Добавить задачу.
-# 3) Удалить заметку."""
-#     print(text)
-#     resp = input("Ввод? => ")
-#     return resp
-
-# def main(keywords, table_name):
-#     try:
-#         resp = start_menu_selector()
-#         if resp == keywords["get_entries"]:
-#             queries = get_entries()
-#             TableIt.printTable(queries, useFieldNames=True)
-
-#         elif resp == keywords["add_entry_to_table"]:
-#             r = input("Введите заметку =>")
-#             add_entry_to_table(datetime.now().strftime("%d %B %Y %I:%M%p"), r)
-
-#         elif resp == keywords["remove_entry"]:
-#             r = input("Введите ИД заметки =>")
-#             remove_entry(int(r))
-#         else:
-#             pass
-#     except KeyboardInterrupt:
-#         print("Выходим...")
-#         exit(0)
-#     return
-# def drawLogo():
-#     print("""   _            _          _ _               _
-#  | |          | |        | (_)             | |
-#  | |_ __ _ ___| | __   __| |_ _ __ ___  ___| |_ ___  _ __
-#  | __/ _` / __| |/ /  / _` | | '__/ _ \/ __| __/ _ \| '__|
-#  | || (_| \__ \   <  | (_| | | | |  __/ (__| || (_) | |
-#   \__\__,_|___/_|\_\  \__,_|_|_|  \___|\___|\__\___/|_|
-
-#                                                           """)
-# if __name__ == '__main__':
-#     keywords = {
-#         'get_entries': '1',
-#         'add_entry_to_table': '2',
-#         'remove_entry': '3'
-#     }
-#     table_name = "table.db"
-#     init_table()
-#     drawLogo()
-#     print("Для выхода нажми Ctrl-C")
-#     while True:
-#         main(keywords, table_name)
